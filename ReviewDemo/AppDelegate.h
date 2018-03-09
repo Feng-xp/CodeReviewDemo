6c190f3ccc8454cc9681bb5c2683f6b24176cebf
qzp <qinzhipeng@jk.cn>
2018-03-09 14:38:34 +0800

    Version: < 工程初始化 >
    Type: < 新功能 | 修复Bug | 重构代码 >
    
    Summary: < 新功能描述 | Bug号和描述 | 重构说明 >
    
    Description: < 详细描述 >

---
+++ ReviewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ReviewDemo
+//
+//  Created by qzp on 2018/3/9.
+//  Copyright © 2018年 qzp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ReviewDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ReviewDemo
+//
+//  Created by qzp on 2018/3/9.
+//  Copyright © 2018年 qzp. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
